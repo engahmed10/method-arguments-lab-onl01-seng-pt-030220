81cb56f7ea279a3104f94e42bd374778fd945c6d
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-02 17:23:05 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,6 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+def introduction_with_language(name1,name2)
+  puts "Hi, my name is #{name1} and I am learning to program in #{name2}."
+end
